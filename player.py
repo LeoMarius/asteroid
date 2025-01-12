ed18d034e96398757bd043af1124a517accfd6e9
LeoMarius <mariusleo64@gmail.com>
2025-01-12 16:11:00 +0100

    Add for projectiles

--- player.py
+++ player.py
@@ -2,6 +2,8 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import *
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -31,6 +33,8 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -38,3 +42,9 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+
+        shot_velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED 
+
+        shot = Shot(self.position.x, self.position.y)
